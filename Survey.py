a2a4016a0c6ae3d610781eec916010138613f2ea
Et-phonehome24 <teshima@uoregon.edu>
2024-10-19 14:29:32 -0700

    Questions Class

--- Survey.py
+++ Survey.py
@@ -1,6 +1,19 @@
 from Question import Question
+import csv
 
 class Survey:
+
+
     def __init__(self, num_of_questions):
         self.num_of_questions = num_of_questions
         self.questions = []
+
+
+
+    def Question_pool(num_of_questions : int):
+        result = {}
+        file = open(PATH, 'r')
+        reader = csv.DictReader(file)
+        for row in reader:
+            read_csv_column(file, "key_field")
+        return result
