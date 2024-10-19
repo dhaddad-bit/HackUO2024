ac0e446cc6b5c271ea71347a192fc2f524a188b1
noahham <noahham25@gmail.com>
2024-10-19 14:48:45 -0700

    test

--- Survey.py
+++ Survey.py
@@ -2,13 +2,9 @@ from Question import Question
 import csv
 
 class Survey:
-
-
     def __init__(self, num_of_questions):
         self.num_of_questions = num_of_questions
         self.questions = []
-
-
 
     def Reader(Path : str) -> []:
         result = []
