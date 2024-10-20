358056486e134c1b4558f472ff2b3366c24f0d1f
caarrp <junebirdsfly@gmail.com>
2024-10-20 09:31:40 -0700

    photos GOODBYE

--- view_3.py
+++ view_3.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 import tkinter.tix as tix
 from tkinter import ttk
-from PIL import Image, ImageTk
+
+#from survey import Survey
 from tkcalendar import Calendar, DateEntry
 import datetime
+from PIL import Image, ImageTk
 
-import csv
-import word2number
+#plot imports
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+
 
 from questions import *
 
-DIC = {}
 
 PAST_RATES = load_data("csv/rate_day.csv", ["date", "rating"])
 QUANT_RES = load_data("csv/quantitative_responses.csv", ["date", "question_#", "response"])
@@ -385,11 +389,11 @@ class InspQuPage(tk.Frame):
         self.back_button.pack(pady=10)
 
         self.label = tk.Label(self, text= rand_quote(load_quotes("csv/inspirational_quotes.csv", "Quote"))[1])
-        self.label.pack(pady=5)
+        self.label.pack(pady=50)
 
         self.quote_button = tk.Button(self, text="New Quote",
                                       command=lambda: self.get_new_quote(self.label))
-        self.quote_button.pack(pady=5)
+        self.quote_button.pack(pady=50)
 
     def get_new_quote(self, label):
         label.config(text=rand_quote(load_quotes("csv/inspirational_quotes.csv", "Quote"))[1])
@@ -438,12 +442,48 @@ class EditPage(tk.Frame):
 
 
 class TrendPage(tk.Frame):
-    def __init__(self, parent, controller):
+     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
     
         self.back_button = tk.Button(self, text="Back to Main Page",
                                      command=lambda: controller.show_frame(StartPage))
         self.back_button.pack(pady=10) 
+
+        self.plot_button = tk.Button(master = self, 
+                     command = lambda: self.plot_rate_day(),
+                     height = 2, 
+                     width = 10,
+                     text = "Plot Rate Day")
+        self.plot_button.pack(pady=10)
+
+     def plot_rate_day(self):
+        # Creating Figure Widget
+        fig = Figure(figsize = (5, 5))
+        # Pull Ratings from File
+        data_list = sorted(load_data("csv/rate_day.csv", ["date", "rating"])) #Sort by datetime
+        y = [data_list[i][0] for i in range(len(data_list))]
+        x = [int(data_list[i][1]) for i in range(len(data_list))]
+        #print(data_list)
+
+        # Adding plots
+        plot1 = fig.add_subplot()
+
+        plot1.plot(y, x)
+
+        # Creating canvass using FigureCanvasTkAgg()
+        canvas = FigureCanvasTkAgg(fig,
+                               master = self)  
+        canvas.draw()
+
+        canvas.get_tk_widget().pack()
+
+        # Creating Toolbar using Matplotlib
+        toolbar = NavigationToolbar2Tk(canvas,
+                                   self)
+        toolbar.update()
+
+        canvas.get_tk_widget().pack()
+
 
 if __name__ == '__main__':
     app = App()
