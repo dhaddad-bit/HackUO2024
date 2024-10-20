ff371a8c71a977e75f7780cac3953f14f0ec787a
Anna Norris <alnorris113@gmail.com>
2024-10-20 08:51:44 -0700

    woooooooo

--- view.py
+++ view.py
@@ -236,6 +236,13 @@ class RateDayPage(tk.Frame):
         DIC["rating"] = str(i)
         self.update_calendar()
 
+        date = datetime.datetime.today().strftime("%Y-%m-%d")
+
+        if search_headercolumn("csv/rate_day.csv", 0, date)[0]:
+            pass
+        else:
+            save_inputs([date, i], "csv/rate_day.csv")
+
 
     def update_calendar(self):
         cal = self.controller.frames[CalendarPage]
