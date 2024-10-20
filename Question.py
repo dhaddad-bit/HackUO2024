0fb9f4febf4764e579b5785b171dec26e526be66
caarrp <junebirdsfly@gmail.com>
2024-10-19 20:34:12 -0700

    no more'

--- Question.py
+++ Question.py
@@ -1,10 +1,8 @@
-from elements import QuElement, QuEvent, EventKindQuestion, Listener
 
 
-class Question(QuElement):
+class Question(object):
 
     def __init__(self, question_text, value : bool):
-        super.__init__()
         self.is_qual = value
         # Question text  is determined later in the Survey class
         self.question_text = question_text
@@ -15,8 +13,6 @@ class Question(QuElement):
 
     def set_question_text(self, new_question_text):
         self.question_text = new_question_text
-        event = QuEvent(EventKindQuestion.question_opened, self)  
-        self.notify_all(event)  # Notify all listeners that a question has been opened
 
 
     def get_question_text(self):
@@ -24,8 +20,6 @@ class Question(QuElement):
 
     def set_response_text(self, new_response_text):
         self.response_text = new_response_text
-        event = QuEvent(EventKindQuestion.question_answered, self)  
-        self.notify_all(event)  
 
     def get_response_text(self):
         return self.response_text
