a910444569f8f14aecac61d5a02a7160c03d6792
caarrp <junebirdsfly@gmail.com>
2024-10-20 01:01:10 -0700

    woooolwop

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 from view import *
 
 
-
-
-def main():
-    app = WellnessTracker()
-    app.mainloop()
-    print
-=======
 from survey import Survey
-from view import WellnessTracker
 
-s = Survey(10, 'quantitative')
-print(s.question_pool())
 
 
 def main():
     s = Survey() 
-    s.reader(location)
-    s.reader(location2)
+
 
 
 
