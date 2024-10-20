21bc081547ac1648b433379c020c299e2ffcdd19
caarrp <junebirdsfly@gmail.com>
2024-10-19 23:24:19 -0700

    weeep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 <<<<<<< HEAD
 import Question
 from view import *
-from elements import QuElement, QuEvent, EventKindQuestion
 
 first_q = Question(False)
 Question.
@@ -30,16 +29,6 @@ def main():
 
 
 
-
-
-
-
-class QuestionListener(Listener):
-    def notify(self, event: QuEvent):
-        if event.kind == EventKindQuestion.question_opened:
-            print(f"Question opened: {event.q.getQuestionText()}")
-        elif event.kind == EventKindQuestion.question_answered:
-            print(f"Question answered: {event.q.getResponseText()}")
 
 
 
